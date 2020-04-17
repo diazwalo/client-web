591e516d6e53cac667fbfcbd7cb36837a1cba95c
Thomas Fritsch <thomas@uidlt.fr>
2020-04-17 20:03:51 +0200

    détection auto protocol & hostname

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-const appContainer = document.querySelector('#appContainer');
+const appContainer = document.querySelector('#appContainer'),
+	hostname = window.location.hostname,
+	protocol = window.location.protocol;
 
-fetch('http://localhost:8080/api/v1/myresource')
+fetch(`${protocol}//${hostname}:8080/api/v1/myresource`)
 	.then(response => response.text())
 	.then(message => (appContainer.innerHTML = message));
