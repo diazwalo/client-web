f465503837cc95b97e616ebae522ddf9e64168dd
Thomas Clavier <tom@tcweb.org>
2020-04-18 22:24:31 +0200

    build url with port

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 const appContainer = document.querySelector('#appContainer'),
 	hostname = window.location.hostname,
 	protocol = window.location.protocol;
+	port = window.location.port;
 
-fetch(`${protocol}//${hostname}:8080/api/v1/myresource`)
+const myresource_url = port === "" ? `${protocol}//${hostname}/api/v1/myresource` : `${protocol}//${hostnamea}:8080/api/v1/myresource`
+
+fetch(myresource_url)
 	.then(response => response.text())
 	.then(message => (appContainer.innerHTML = message));
