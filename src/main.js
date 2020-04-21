97f0f1ed8014c1af6b617a72a1980a87fda235ed
Thomas Clavier <tom@tcweb.org>
2020-04-21 18:19:38 +0200

    Destructuring en param de fonction et pas dans le main ... end

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import getApiUrl from './utils/url';
 
 const appContainer = document.querySelector('#appContainer'),
-    const {protocol, hostname, port} = window.location;
-	apiUrl = getApiUrl(protocol, hostname, port);
+	apiUrl = getApiUrl(window.location);
 
 fetch(apiUrl + '/api/v1/myresource')
 	.then(response => response.text())
