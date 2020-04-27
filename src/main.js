7e86699d13833bbf5daa2f1849582e6e95e3807b
Lezenn <lezenn@lezenn-OMEN.home>
2020-04-27 16:35:47 +0200

    Ajustement pour hello-world et correction titre page

--- src/main.js
+++ src/main.js
@@ -4,13 +4,23 @@ import Component from './rendering/Component';
 const appContainer = document.querySelector('#appContainer'),
 	apiUrl = getApiUrl(window.location);
 
-fetch(apiUrl + '/api/v1/myresource')
+fetch(apiUrl + '/api/v1/helloworld')
 	.then(response => response.text())
-	.then(message => (appContainer.innerHTML = message))
+	.then(message => {
+		// Si le message du serveur est bien reçu
+		const messageComponent = new Component(
+			'h1',
+			{ style: 'color: #096fe3; text-shadow: 1px 1px 12px #5c5c5c' },
+			message,
+			false
+		);
+		appContainer.innerHTML = messageComponent.render();
+	})
 	.catch(error => {
+		// Si le serveur n'est pas joignable
 		const errorMessage = new Component(
 			'h1',
-			{ style: 'color: red' },
+			{ style: 'color: red; font-size: 11pt' },
 			"Erreur lors de l'obtention d'informations depuis le serveur: " + error,
 			false
 		);
