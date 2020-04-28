f7ef27cb9d3a1d8b7cb010fc6266873df8d97b2f
Lezenn <lezenn@lezenn-OMEN.home>
2020-04-28 14:17:45 +0200

    Modele de page de test

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,57 @@
 import Component from '../rendering/Component';
+import Card from '../rendering/Card';
 
 export default class Index extends Component {
 	constructor() {
 		super('main');
 		const newContent = [
-			new Component('h1', { class: 'text-teal-500 text-xl' }, 'Accueil'),
+			new Component(
+				'section',
+				[
+					{
+						class:
+							'bg-gray-800 pt-2 pb-2 border-solid border-b-4 border-teal-500 mb-2',
+					},
+				],
+				[
+					new Component('img', [
+						{ src: 'images/Logo_green_Water.png' },
+						{ alt: 'greenwater-logo' },
+						{ class: 'm-auto w-64 h-32' },
+					]),
+					new Component(
+						'p',
+						[{ class: 'text-center text-white' }],
+						'atum deleniti atque corrupti quos dolores et quas molestias e'
+					),
+				]
+			),
+			// Cards
+			new Component(
+				'div',
+				[{ class: 'flex flex-row flex-wrap justify-around mb-4' }],
+				[
+					new Card(
+						'Hello World !',
+						`At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
+                voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati`,
+						'https://i.picsum.photos/id/1002/512/512.jpg'
+					),
+					new Card(
+						'Hello World !',
+						`At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
+                voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati`,
+						'https://i.picsum.photos/id/1002/512/512.jpg'
+					),
+					new Card(
+						'Hello World !',
+						`At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
+                voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati`,
+						'https://i.picsum.photos/id/1002/512/512.jpg'
+					),
+				]
+			),
+
 			new Component(
 				'p',
 				{ class: 'text-gray-700' },
