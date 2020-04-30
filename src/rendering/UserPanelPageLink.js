45d1b64d07735e677b127a4c22b5e5317c4011e4
Lezenn <lezenn@lezenn-OMEN.home>
2020-04-30 14:15:09 +0200

    Ajout component menu userpanel

---
+++ src/rendering/UserPanelPageLink.js
@@ -0,0 +1,22 @@
+import Component from './Component';
+
+export default class UserPanelPageLink extends Component {
+	/**
+	 * Constructor
+	 * @param {string} title
+	 * @param {string} id
+	 */
+	constructor(title, id) {
+		super(
+			'h3',
+			[
+				{
+					class:
+						'text-gray-500 hover:text-blue-300 text-lg cursor-pointer text-center mb-4',
+				},
+				{ id: id },
+			],
+			title
+		);
+	}
+}
